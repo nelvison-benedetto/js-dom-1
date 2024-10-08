434c62c1785cacd49ee217b09827b1c284f6c4ab
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2024-10-08 17:24:51 +0200

    done

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const btn_lamp = document.querySelector("div button");
 const img_lamp = document.getElementById("img_lamp"); //faster than querySelector("#...")
 
+isOn = false;
 function turnOnOff() {
-    console.log("on off test");
-    img_lamp.src = "./assets/img/yellow_lamp.png";
+    //img_lamp.src = "./assets/img/yellow_lamp.png";
+
+    if (!isOn){ isOn=true; img_lamp.src= "./assets/img/yellow_lamp.png";}
+    else { isOn=false; img_lamp.src= "./assets/img/white_lamp.png";}
 }
 
-btn_lamp.addEventListener('click', turnOnOff);
+btn_lamp.addEventListener("click", turnOnOff);
